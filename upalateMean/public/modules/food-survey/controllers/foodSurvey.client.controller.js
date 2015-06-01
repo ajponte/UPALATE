9b82d641ed1b6ffdad47fffabf0fb93a146431bc
= <alanjponte@gmail.com>
2015-05-31 18:23:00 -0700

    cleaning merge conflict

--- upalateMean/public/modules/food-survey/controllers/foodSurvey.client.controller.js
+++ upalateMean/public/modules/food-survey/controllers/foodSurvey.client.controller.js
@@ -4,34 +4,6 @@
 /** Angular controller for the food survey.
  *  @author Alan Ponte
  */
-<<<<<<< HEAD
-angular.module('food-survey').controller('foodSurveyController', ['$scope', '$http', 'Authentication',
-	function($scope, $http, Authentication) {
-
-		$scope.dates = {};
-
-		$scope.Authentication = Authentication;
-		/** Returns today current date. */
-		$scope.dates.currentDate = (function() {
-			var today = new Date();
-			var dd = today.getDate();
-			var mm = today.getMonth() + 1;
-			var yyyy = today.getFullYear();
-			if (dd < 10) {
-				dd = '0' + dd;
-			}
-			if (mm < 10) {
-				mm = '0' + mm;
-			}
-			today = mm + '/' + dd + '/' + yyyy;
-			return today;
-		})();
-
-		/** Returns yesterdays date. */
-		$scope.dates.yesterday = (function() {
-			var today = new Date();
-			var day = (today.getDay() - 2) % 7;
-=======
 angular.module('food-survey').controller('foodSurveyController', ['$scope', '$http', '$resource','$window', '$location', 'Authentication',
 	function($scope, $http, $resource, $window, $location, Authentication) {
 	var tags = $resource('foodNames.json');
@@ -54,7 +26,6 @@ angular.module('food-survey').controller('foodSurveyController', ['$scope', '$ht
 			var today = new Date();
 			var day = today.getDay();
 			console.log("yesterday: " + day);
->>>>>>> origin/imtootired
 			var dd = today.getDate() - 1;
 			var mm = today.getMonth() + 1;
 			var yyyy = today.getFullYear();
@@ -65,15 +36,6 @@ angular.module('food-survey').controller('foodSurveyController', ['$scope', '$ht
 				mm = '0' + mm;
 			}
 			today = mm + '/' + dd + '/' + yyyy;
-<<<<<<< HEAD
-			return getDayString(day) + ' ' + today;
-		})();
-
-		/** Returns the day before yesterday's date. */
-		$scope.dates.dayBeforeYesterday = (function() {
-			var today = new Date();
-			var day = (today.getDay() - 3) % 7;
-=======
 			return getDayString(day, "yesterday") + ' ' + today;
 		})();
 
@@ -81,7 +43,6 @@ angular.module('food-survey').controller('foodSurveyController', ['$scope', '$ht
 		$scope.dayBeforeYesterday = (function() {
 			var today = new Date();
 			var day = today.getDay();
->>>>>>> origin/imtootired
 			var dd = today.getDate() - 2;
 			var mm = today.getMonth() + 1;
 			var yyyy = today.getFullYear();
@@ -92,24 +53,14 @@ angular.module('food-survey').controller('foodSurveyController', ['$scope', '$ht
 				mm = '0' + mm;
 			}
 			today = mm + '/' + dd + '/' + yyyy;
-<<<<<<< HEAD
-			return getDayString(day) + ' ' + today;
-=======
 			return getDayString(day, "dayBefore") + ' ' + today;
->>>>>>> origin/imtootired
 		})();
 
 		/** Returns a String of the date two days 
 		 *  ago from today. */
-<<<<<<< HEAD
-		$scope.dates.twoDaysAgo = (function() {
-			var today = new Date();
-			var day = (today.getDay() - 4) % 7;
-=======
 		$scope.twoDaysAgo = (function() {
 			var today = new Date();
 			var day = today.getDay() % 7;
->>>>>>> origin/imtootired
 			var dd = today.getDate() - 2;
 			var mm = today.getMonth() + 1;
 			var yyyy = today.getFullYear();
@@ -120,21 +71,13 @@ angular.module('food-survey').controller('foodSurveyController', ['$scope', '$ht
 				mm = '0' + mm;
 			}
 			today = mm + '/' + dd + '/' + yyyy;
-<<<<<<< HEAD
-			return getDayString(day) + ' ' + today;
-=======
 			return getDayString(day, "twoDaysBefore") + ' ' + today;
->>>>>>> origin/imtootired
 		})();
 
 		/** Returns the String representation of 
 		 *  the day DD. */
-<<<<<<< HEAD
-		 function getDayString(dd) {
-=======
 		 function getDayString(dd, whichDay) {
 		 	console.log("dd: " + dd);
->>>>>>> origin/imtootired
 			var weekday = new Array(7);
 			weekday[0]="Monday";
 			weekday[1]="Tuesday";
@@ -143,8 +86,6 @@ angular.module('food-survey').controller('foodSurveyController', ['$scope', '$ht
 			weekday[4]="Friday";
 			weekday[5]="Saturday";
 			weekday[6]="Sunday";
-<<<<<<< HEAD
-=======
 			if (whichDay === "yesterday") {
 				if (dd === 0) {
 					return weekday[5];
@@ -160,29 +101,11 @@ angular.module('food-survey').controller('foodSurveyController', ['$scope', '$ht
 					return weekday[3];
 				}
 			}
->>>>>>> origin/imtootired
 			return weekday[dd];
 		};
 
 		/** Submits the survey to Mongo. */
 		$scope.submitSurvey = function() {
-<<<<<<< HEAD
-			var survey = {
-				yesterday: {
-					breakfast: $scope.yesterdayBreakfast,
-					lunch: $scope.yesterdayLunch,
-					dinner: $scope.yesterdayDinner
-				},
-				dayBeforeYesterday: {
-					breakfast: $scope.dayBeforeYesterdayBreakfast,
-					lunch: $scope.dayBeforeYesterdayLunch,
-					dinner: $scope.dayBeforeYesterdayDinner
-				},
-				twoDaysBeforeYesterday: {
-					breakfast: $scope.twoDaysbeforeYesterdayBreakfast,
-					lunch: $scope.twoDaysbeforeYesterdayLunch,
-					dinner: $scope.twoDaysbeforeYesterdayDinner
-=======
 			console.log("yesterday: " + JSON.stringify($scope.yesterdayBreakfast));
 			var survey = {
 				yesterday: {
@@ -256,7 +179,6 @@ angular.module('food-survey').controller('foodSurveyController', ['$scope', '$ht
 						}
 						return arr;
 					})()
->>>>>>> origin/imtootired
 				},
 				user: $scope.Authentication.user._id
 			};
@@ -265,10 +187,8 @@ angular.module('food-survey').controller('foodSurveyController', ['$scope', '$ht
 				.success(function(data) {
 					console.log("data from post: " + JSON.stringify(data));
 				});
-<<<<<<< HEAD
-=======
+
 			$location.path('/settings/profile');
->>>>>>> origin/imtootired
 		};
 
 	}
