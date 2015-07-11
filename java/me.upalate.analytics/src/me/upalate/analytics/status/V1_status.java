06f9db99dbbe995c25201a58371d1df205fbfd9d
axuan <axuan@chrisplusplus.net>
2015-07-11 16:01:21 +0800

    Jersey import + V1 class

---
+++ java/me.upalate.analytics/src/me/upalate/analytics/status/V1_status.java
@@ -0,0 +1,15 @@
+package me.upalate.analytics.status;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/status")
+public class V1_status {
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle() {
+		return "<p>Java Web Services</p>";
+	}
+	
+}
