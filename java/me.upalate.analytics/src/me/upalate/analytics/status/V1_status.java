9b455d2a5ef30e6acee2ab408354b343d0e46bd8
axuan <axuan@chrisplusplus.net>
2015-07-14 11:22:07 +0800

    DAO class; jdbc/MongoDB connection pool

--- java/me.upalate.analytics/src/me/upalate/analytics/status/V1_status.java
+++ java/me.upalate.analytics/src/me/upalate/analytics/status/V1_status.java
@@ -1,14 +1,70 @@
 package me.upalate.analytics.status;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.*;
+
+import java.sql.*;
+
+import me.upalate.dao.*;
 
 @Path("/v1/status")
 public class V1_status {
 
+	private static final String api_version = "0.0.1";
+	
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String returnTitle() {
 		return "<p>Java Web Service</p>";
+	}
+	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion() {
+		return "<p>Version</p>"+ api_version;
+	}
+	
+	@Path("/database")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnDatabaseStatus() throws Exception {
+		
+		PreparedStatement query = null;
+		StringBuilder output = (StringBuilder) new StringBuilder("DB TEST: ");
+		Connection conn = null;
+		
+		try {
+			
+			conn = UPalateAnalytics.UPalateAnalyticsConn().getConnection();
+			query = conn.prepareStatement("SELECT * FROM system.indexes");
+			ResultSet rs = query.executeQuery();
+			
+			output.append(rs.getMetaData());
+//			
+//			
+//			while (rs.next()) {
+//				for(int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+//					output.append(rs.getMetaData().getColumnName(i) + "=" + rs.getString(i));
+//				}
+//				
+////				myString = rs.getString("name");
+//			}
+//			
+//			output.append("ROWS: ").append(rs.toString());
+//			query.close();
+//			
+////			returnString = "DB first food name: " + myString;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				conn.close();
+			}
+		}
+		
+//		return returnString;
+		return output.toString();
 	}
 }
---
+++ java/me.upalate.analytics/src/me/upalate/dao/UPalateAnalytics.java
@@ -0,0 +1,33 @@
+package me.upalate.dao;
+
+import javax.naming.*;
+import javax.sql.*;
+
+/*
+ * TODO: implement logging
+ */
+public class UPalateAnalytics {
+
+	private static DataSource UPalateAnalytics = null;
+	private static Context context = null;
+	
+	public static DataSource UPalateAnalyticsConn() throws Exception {
+		
+		if (UPalateAnalytics != null) {
+			return UPalateAnalytics;
+		}
+		
+		try {
+			if (context == null) {
+				context = new InitialContext();
+			}
+			
+			UPalateAnalytics = (DataSource) context.lookup("jdbc/MongoDB");
+			
+		} catch (Exception e) {
+			e.printStackTrace(); // todo: error catching
+		}
+		
+		return UPalateAnalytics;
+	}
+}
